b3ccabb044e2cb9fe6b3d6c60d8db27b1c7f053d
Robin Kelsick II <robinkelsick@gmail.com>
2023-05-16 21:51:19 -0400

    this is the first version of the final product

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -8,7 +8,6 @@ const Calculator = () => {
     const [timeDiff, setTimeDiff] = useState(null);
     const [err, setErr] = useState('');
 
-    const inputDate = new Date(yearValue, monthValue - 1, dayValue); 
 
     const handleDay = (e) => {
         setDayValue(e.target.value);
@@ -25,14 +24,22 @@ const Calculator = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (!inputDate) {
-            setErr("Please enter a date.");
+        const inputDate = new Date(yearValue, monthValue - 1, dayValue); 
+
+
+        if (!dayValue || !monthValue || !yearValue){
+            setErr('Please enter a complete date.');
+            return;
+        }
+
+        if (isNaN(inputDate.getTime())){
+            setErr('Please enter a valid date.');
             return;
         }
 
         const diff = calculateTimeDiff(inputDate);
 
-        setErr('');
+        setErr("");
         setTimeDiff(diff);
     }
 
@@ -70,57 +77,73 @@ const Calculator = () => {
     console.log(timeDiff);
 
     return (
-      <div className="flex justify-center items-center bg-[rgb(240,240,240)] w-full h-[100vh]">
-        <div className="bg-white h-[650px] w-[840px] m-auto rounded-br-[250px] rounded-[40px]">
-          <div className="flex flex-row mt-14 ml-[60px]">
+      <div className="flex flex-col items-center bg-[rgb(240,240,240)] w-full min-h-screen">
+        <h1 className="text-center mt-4 text-7xl sm:text-8xl">Age Calculator</h1>
+        <div className="bg-white max-w-[520px] m-auto mt-4 sm:min-w-[840px] sm:mx-0 rounded-br-[70px] sm:rounded-br-[250px] rounded-[20px] sm:rounded-[40px]">
+          <div className="flex flex-row w-1/2 mt-4 sm:mt-14 ml-4 sm:ml-[60px]">
             <form onSubmit={handleSubmit}>
-              <div className="flex flex-row">
-                <label htmlFor="dayInput">
-                  <p className="uppercase text-lg font-semibold text-[#716f6f]">
-                    day
-                  </p>
-                  <input
-                    type="text"
-                    value={dayValue}
-                    placeholder="DD"
-                    onChange={handleDay}
-                    className="border ${err ? 'border-red-500' : 'border-[#dbdbdb]'}  focus:outline-none rounded-lg h-[72px] w-[156px] mr-8 px-4 py-2"
-                  />
-                </label>
-                <label htmlFor="monthInput">
-                  <p className="uppercase text-lg font-semibold text-[#716f6f]">
-                    month
-                  </p>
-                  <input
-                    type="text"
-                    value={monthValue}
-                    placeholder="MM"
-                    onChange={handleMonth}
-                    className={`border ${err ? 'border-red-500' : 'border-[#dbdbdb]'} focus:outline-none rounded-lg h-[72px] w-[156px] mr-8 px-4 py-2`}
-                  />
-                </label>
-                <label htmlFor="yearInput">
-                  <p className="uppercase text-lg font-semibold text-[#716f6f]">
-                    year
-                  </p>
-                  <input
-                    type="text"
-                    value={yearValue}
-                    placeholder="YYYY"
-                    onChange={handleYear}
-                    className="border ${err ? 'border-red-500' : 'border-[#dbdbdb]'} focus:outline-none rounded-lg h-[72px] w-[156px] px-4 py-2"
-                  />
-                </label>
+              <div className="flex flex-row sm:justify-between">
+                <div className="flex flex-col">
+                  <label htmlFor="dayInput">
+                    <p className="uppercase text-sm font-semibold text-[#716f6f]">
+                      day
+                    </p>
+                    <input
+                      type="text"
+                      value={dayValue}
+                      placeholder="DD"
+                      onChange={handleDay}
+                      className={`border ${
+                        err ? "border-red-500" : "border-[#dbdbdb]"
+                      }  focus:outline-none rounded-lg h-[72px] w-full mr-8 px-4 py-2`}
+                    />
+                  </label>
+                  {err ? <p className="text-xs text-red-500">{err}</p> : ""}
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="monthInput">
+                    <p className="uppercase text-sm font-semibold text-[#716f6f]">
+                      month
+                    </p>
+                    <input
+                      type="text"
+                      value={monthValue}
+                      placeholder="MM"
+                      onChange={handleMonth}
+                      className={`border ${
+                        err ? "border-red-500" : "border-[#dbdbdb]"
+                      } focus:outline-none rounded-lg h-[72px] w-full mr-8 px-4 py-2`}
+                    />
+                  </label>
+                  {err ? <p className="text-xs text-red-500">{err}</p> : ""}
+                </div>
+                <div className="flex flex-col">
+                  <label htmlFor="yearInput">
+                    <p className="uppercase text-sm font-semibold text-[#716f6f]">
+                      year
+                    </p>
+                    <input
+                      type="text"
+                      value={yearValue}
+                      placeholder="YYYY"
+                      onChange={handleYear}
+                      className={`border ${
+                        err ? "border-red-500" : "border-[#dbdbdb]"
+                      } focus:outline-none rounded-lg h-[72px] w-full px-4 py-2`}
+                    />
+                  </label>
+                  {err ? <p className="text-xs text-red-500">{err}</p> : ""}
+                </div>
               </div>
               <div className="flex flex-row">
-                <div className="border-b-2 border-gray-200 h-[50px] w-[620px]"></div>
+                <div className="border-b-2 border-gray-200 w-[250px] sm:w-[620px]"></div>
                 <button type="submit">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="75"
                     height="75"
                     viewBox="0 0 46 44"
-                    className="bg-[#854dff] rounded-full"
+                    className="bg-[#854dff] rounded-full mt-4"
                   >
                     <g
                       fill="none"
@@ -135,32 +158,32 @@ const Calculator = () => {
               </div>
             </form>
           </div>
-          <div className="flex flex-col ml-[60px]">
-            {timeDiff === null ? (
-              <h1 className="text-8xl font-extrabold">
-                <span className="text-[#854dff]">--</span>Years
+          <div className="flex flex-col m-auto sm:ml-[60px]">
+            {timeDiff === null || err ? (
+              <h1 className="text-7xl sm:text-8xl font-extrabold">
+                <span className="text-[#854dff]">--</span>years
               </h1>
             ) : (
-              <h1 className="text-8xl font-extrabold">
-                <span className="text-[#854dff]">{timeDiff.years}</span>Years
+              <h1 className="text-7xl sm:text-8xl font-extrabold">
+                <span className="text-[#854dff]">{timeDiff.years}</span>years
               </h1>
             )}
-            {timeDiff === null ? (
-              <h1 className="text-8xl font-extrabold">
-                <span className="text-[#854dff]">--</span>Months
+            {timeDiff === null || err ? (
+              <h1 className="text-7xl sm:text-8xl font-extrabold">
+                <span className="text-[#854dff]">--</span>months
               </h1>
             ) : (
-              <h1 className="text-8xl font-extrabold">
-                <span className="text-[#854dff]">{timeDiff.months}</span>Months
+              <h1 className="text-7xl sm:text-8xl font-extrabold">
+                <span className="text-[#854dff]">{timeDiff.months}</span>months
               </h1>
             )}
-            {timeDiff === null ? (
-              <h1 className="text-8xl font-extrabold">
-                <span className="text-[#854dff]">--</span>Days
+            {timeDiff === null || err ? (
+              <h1 className="text-7xl sm:text-8xl font-extrabold">
+                <span className="text-[#854dff]">--</span>days
               </h1>
             ) : (
-              <h1 className="text-8xl font-extrabold">
-                <span className="text-[#854dff]">{timeDiff.days}</span>Days
+              <h1 className="text-7xl sm:text-8xl font-extrabold mb-2 md:mb-4 px-1">
+                <span className="text-[#854dff]">{timeDiff.days}</span>days
               </h1>
             )}
           </div>
